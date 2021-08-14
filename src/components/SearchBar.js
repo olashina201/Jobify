79f2fcfd267b08a6b9922aacd7e9d1dc0a1537e6
olashina201 <69033389+olashina201@users.noreply.github.com>
2021-08-14 13:38:44 +0100

    new update

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from "react";
-import axios from"axios";
+import React from "react";
 import "../assets/styles.css";
 
 const SearchBar = ({ searchJob, handleSubmit, setSearchJob }) => {
